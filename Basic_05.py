57f6da235e58b1531ba9f948eebf3c7fced78a53
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-05 22:54:10 +0530

    Basic_Programs

--- Basic_05.py
+++ Basic_05.py
@@ -1,10 +1,16 @@
-# simple interset for given principal amount, time and 
-#rate of interset. 
-def simple_interest(p,t,r):
-    print('Then principle is',p)
-    print('The time period is',t)
-    print('The rate of interest',r)
-    si =(p*t*r)/100
-    print('The Simple Interest is',si)
-    return si
-simple_interest(8,6,8)
+#Program to check if a number is prime or not.
+num =29
+# To take input from the user
+num =int(input("Enter number:"))
+# define a flag variable
+flag =False
+# prime numners are greater than 1
+if num >1:
+    for i in range(2,num):
+        if(num % i)==0:
+            flag = True
+            break
+if flag:
+    print(num,"is not a prime number")
+else:
+    print(num,"is a prime number")
