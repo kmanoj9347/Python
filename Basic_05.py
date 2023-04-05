0ef34eff6820eb30f4991b85931bbd4050bf02e1
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-05 22:14:59 +0530

    Basic_Programs

---
+++ Basic_05.py
@@ -0,0 +1,10 @@
+# simple interset for given principal amount, time and 
+#rate of interset. 
+def simple_interest(p,t,r):
+    print('Then principle is',p)
+    print('The time period is',t)
+    print('The rate of interest',r)
+    si =(p*t*r)/100
+    print('The Simple Interest is',si)
+    return si
+simple_interest(8,6,8)
--- Basic_Programs/Basic_01.py
+++ Basic_Programs/Basic_01.py
@@ -1,3 +1,4 @@
+# maximum of two numbers using max
 a=2
 b=4
 maximum =max(a,b)
