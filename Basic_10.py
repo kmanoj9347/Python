a8e8e31aabf89c68df558638005b8f23f8685c58
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-08 11:27:03 +0530

    Basic_programs

---
+++ Basic_10.py
@@ -0,0 +1,8 @@
+#initializing the string
+string  ="I am python programming"
+# splitting the string on space 
+words =string.split()
+## reverse  the words  using reversed() function
+words = list(reversed(words))
+## joining the words and printing
+print(" ".join(words))
