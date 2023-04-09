dafe59b8c823ea908d1201df8e5c45d11311c116
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-09 20:01:18 +0530

    DataTypes

---
+++ Basic_Programs/Data_Types/DataType.py
@@ -0,0 +1,63 @@
+# str
+x ="Hello World"
+print(type(x))
+
+# int
+x =50
+print(type(x))
+
+# float
+x =60.5
+print(type(x))
+
+# complex
+x = 3j
+print(type(x))
+
+# list
+x =["Hai","Hello"]
+print(type(x))
+
+# tuple
+x =("Ravi","Raju","Raja")
+print(type(x))
+
+#range
+x = range(10)
+print(type(x))
+
+#dict
+x ={"Name": "MAnoj","Age": 19}
+print(type(x))
+
+#set
+x ={"Ravi","Raju","Raja"}
+print(type(x))
+
+#frozenset
+x = frozenset({"Ravi","Raju","Raja"})
+print(type(x))
+
+#bool
+x =True
+print(type(x))
+
+# byte
+x =b"Hello"
+print(type(x))
+
+# bytearray
+x =bytearray(4)
+print(type(x))
+
+# memoryview
+x =memoryview(bytes(6))
+print(type(x))
+
+#none
+x =None
+print(type(x))
+
+
+
+
