6e9592ed96373b6b572118c17abd9171ef63476f
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-09 20:15:49 +0530

    List

---
+++ Data_Types/List.py
@@ -0,0 +1,18 @@
+#creating a List
+List = []
+print("initial blank List:",List)
+
+# Creating a list with the use of a string
+List = ['Good Morning']
+print("\nList with the use of String: ",List)
+
+# Creating a list with the use of multiple values
+List = ["John","Tom","Nick"]
+print("\nList containig multiple values: ",List)
+print(List[0])
+print(List[2])
+
+#Creating a Multi-Dimensional List
+#(By Nesting a list inside a list)
+List = [['John','Tom'],['Nick']]
+print("\nMulti-Dimensional List: ",List)
