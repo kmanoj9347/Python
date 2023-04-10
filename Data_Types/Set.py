52d228f20b01b7d44a0bb0f95c4ca80618ae7f88
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-10 18:59:02 +0530

    Set

---
+++ Data_Types/Set.py
@@ -0,0 +1,17 @@
+# Creating a set
+set1 = set()
+print("Initial bllank Set: ")
+print(set1)
+
+#Creating a Set with the use of a String
+set1 = set("Marvelous")
+print("\nSet with the use of String: ",set1)
+
+#Creating a set with the use of a set
+set1 = set(["Morning","Afternoon","Evening","Night"])
+print("\nSet with the use of list: ",set1)
+
+#Creating a set with a mixed type of values
+#(Having numbers and strings)
+set1 = set([1,2,'Ravi',3,'raju,',6,'Ram'])
+print("\nSet with the use of Mixed Values: ",set1)
