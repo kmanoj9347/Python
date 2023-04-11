920721e77e5843eb4a73b8ed29e32ac38ac78f7d
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-11 19:58:17 +0530

    if

---
+++ ConditionalStatement/If.py
@@ -0,0 +1,9 @@
+a =int(input("Enter a: "))
+b =int(input("Enter b: "))
+c =int(input("Enter c: "))
+if a >b and a >c:
+    print("A is Largest");
+if b > a and b >c:
+    print("b is Largest");
+if c > a and c >b:
+    print("c is Largest");
