ea930aba1e1389a85e085d83047b84e72379ac63
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-13 22:26:06 +0530

    Basic_Programs

---
+++ Basic_Programs/Basic_13.py
@@ -0,0 +1,14 @@
+#Character is vowel or consonant
+#FUNction to check wheather a character
+# is Vowel or not
+def vowelOrConsonant(x):
+    if (x =='a' or x =='e' or x =='i'or
+        x =='o' or x =='u' or x =='A' or x =='E' or x =='I'or
+        x =='O' or x =='U'):
+        print("Vowel")
+    else:
+        print("Consonant")
+vowelOrConsonant('e')
+vowelOrConsonant('v')
+vowelOrConsonant('B')
+vowelOrConsonant('I')
