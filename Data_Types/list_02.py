7a6abaf0a787d1928feb3c186d731f21a0a3cb67
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-19 22:28:39 +0530

    Tuple

---
+++ Data_Types/list_02.py
@@ -0,0 +1,28 @@
+#creating a list
+list = []
+print("Blank list: ")
+print(list)
+ 
+#creating a list of numbers
+list = [10,20,30]
+print("\nlist of numbers: ")
+print(list)
+
+#creating a list of strings and accessing
+#using index
+list = ["python","Programming","language"]
+print("\nlist items: ")
+print(list[0])
+print(list[-1])
+
+ #getting the size of list
+print("\nsize of list: ")
+print(len(list))
+
+
+#Addition of elements in the list
+list.append(1)
+list.append('is')
+list.append(1)
+print("\nlist after Addition of three elements: ")
+print(list)
