4a727af57c417730bf7a4241b9199cb9dd64be3b
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-22 23:48:07 +0530

    Tuple

--- Data_Types/Tuple_02.py
+++ Data_Types/Tuple_02.py
@@ -12,3 +12,10 @@ print(tuple1)
 list = [1,2,3,4,5]
 print("\ntuple using list type: ")
 print(tuple(list))
+
+#use of loop
+tuple1 = ('Manoj')
+n =5 
+for i in range(int(n)):
+    tuple1 = (tuple1,)
+    print(tuple1)
