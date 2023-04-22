24bdefff2bc23e1e2263a55b0793178ee0c7ad25
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-22 23:45:32 +0530

    Tuple

---
+++ Data_Types/Tuple_02.py
@@ -0,0 +1,14 @@
+#Creating an empty tuple
+tuple1 = ()
+print("Intial empty tuple: ")
+print(tuple1)
+
+#Creating a tuple with the use of string
+tuple1 = ('python','language')
+print("\ntuple with the the use of string: ")
+print(tuple1)
+
+#use of list
+list = [1,2,3,4,5]
+print("\ntuple using list type: ")
+print(tuple(list))
