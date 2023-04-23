d5c934cc0e15034bbf41a6252cde12f99a1418c4
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-23 22:31:13 +0530

    Modules

---
+++ Recursion/Module.py
@@ -0,0 +1,43 @@
+#importing built-in module math
+import math 
+
+print("square root of 25 is",math.sqrt(25))
+
+print(math.pi)
+
+
+print(math.degrees(1))
+
+#60 degrees = 1.04 radians
+print(math.radians(60))
+
+#sine of 2 radians
+print(math.sin(2))
+
+#cos of 2 radians
+print(math.cos(2))
+
+#tan of 2 radians
+print(math.tan(2))
+
+print(math.factorial(4))
+
+#importing  built in module random
+import random
+
+print(random.randint(0,5))
+
+print(random.random)
+
+print(random.random()*100)
+
+list = [1,4,True,800,"python",27,"hello"]
+
+print(random.choice(list))
+
+#importing built in module datetime
+import datetime
+from datetime import date
+import time
+print(time.time())
+
