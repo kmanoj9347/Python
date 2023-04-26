88b0f62f99eb98a39d4f38f822885ae78201a8ab
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-26 20:05:37 +0530

    SET

--- Data_Types/Set_02.py
+++ Data_Types/Set_02.py
@@ -19,3 +19,15 @@ print(set1)
 set1 = set(["Python","Language","DATATYPE"])
 print("\nSet with the use of List: ")
 print(set1)
+
+#Creating a set with a list of Numbers 
+#(Having duplicate values)
+set1 = set([1,2,4,4,3,3,3,6,5])
+print("\nSet with the use of numbers: ")
+print(set1)
+
+#Creating a set with a mixed type of values
+(Having numbers and strings)
+set1 = set([1,2,'python',4,'language',6,'datatype'])
+print("\nSet with the use of Mixed Values")
+print(set1)
