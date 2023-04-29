c223261661343e1f897e91e69772ffe2ed4bcdb5
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-29 16:38:32 +0530

    Dictionary

--- Data_Types/Dictionary-02.py
+++ Data_Types/Dictionary-02.py
@@ -7,3 +7,11 @@ print(Dict)
 Dict = {'Name':'Manoj',1:[1,2,3,4]}
 print("\nDictinary with the use of Mixed keys: ")
 print(Dict)
+
+#Adding elements 
+Dict[0] ='Zero'
+Dict[4]='Four'
+Dict[1]= 'Numbers'
+Dict['Value_set']='a','b','c'
+print("\nDictionary after adding elements: ")
+print(Dict)
