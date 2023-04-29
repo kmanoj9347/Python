d582f022dffdd26b368498f2a98694e6eecf0df1
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-29 16:28:30 +0530

    Dictionary

---
+++ Data_Types/Dictionary-02.py
@@ -0,0 +1,9 @@
+#Creating a Dictionary with Integer keys
+Dict = {1:'one',2:'two',3:'three'}
+print("\nDictionary with the use of Integer keys: ")
+print(Dict)
+
+#Creating a Dictionary with Mixed keys
+Dict = {'Name':'Manoj',1:[1,2,3,4]}
+print("\nDictinary with the use of Mixed keys: ")
+print(Dict)
