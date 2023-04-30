0e20392d7c6709f2eb89a4593f7230d01979aba8
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-04-30 08:50:43 +0530

    ARRAY

---
+++ Data_Types/Array_01.py
@@ -0,0 +1,15 @@
+#importing an array for array creations
+import array as arr
+#Creating an array with integer type
+a = arr.array('i',[1,2,3])
+print("The new created array is : ",end=" ")
+for i in range(0,3):
+    print(a[i],end=" ")
+
+#creating an array with double type
+b = arr.array('d',[2.5,3.2,3.3])
+print("\nThe new created array is : ",end=" ")
+for i in range(0,3):
+    print(b[i],end=" ")
+
+
