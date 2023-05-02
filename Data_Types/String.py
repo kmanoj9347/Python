c51ce4d08bf962fb6f19a4def20580072a5692e5
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-05-02 19:35:18 +0530

    String

---
+++ Data_Types/String.py
@@ -0,0 +1,17 @@
+String1 ='Welcome to the world'
+print("String withu use of single quotes: ")
+print(String1)
+
+String1 ="I am Manoj"
+print("\nString with the use of double quotes: ")
+print(String1)
+
+String1 ='''\nString with triple Quotes'''
+print(String1)
+print()
+
+String1 ='''k
+        Manoj
+        kumar'''
+print("\nCreating a multiline string: ")
+print(String1)
