0df8e664b0235e20cee68ffb07a950b99ea96268
manoj <khamitkarmanoj@gmail.com>
2023-05-14 11:35:48 +0530

    Default_Constructor

---
+++ OOP/constructor_01.py
@@ -0,0 +1,11 @@
+class Name:
+    #defualt constructor
+    def __init__(self):
+        self.myname ='Manoj kumar'
+    # a method for printinh data members
+    def print_myname(self):
+        print(self.myname)
+#creating object of the class
+obj = Name()
+#calling the instance method using the object obj
+obj.print_myname()
