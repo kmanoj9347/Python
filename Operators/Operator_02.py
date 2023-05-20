c210749c0a03e40af84ff2101749438f872a40b2
manoj <khamitkarmanoj@gmail.com>
2023-05-20 23:33:03 +0530

    Relational_Operators

---
+++ Operators/Operator_02.py
@@ -0,0 +1,9 @@
+#Relational Operators
+a = 9
+b = 5
+print(a>b) #Greater than
+print(a<b) #Less than
+print(a==b) #Equal to
+print(a!=b) #Not equal to 
+print(a>=b) #Greater than or equal to
+print(a<=b) #Less than or Equal to
