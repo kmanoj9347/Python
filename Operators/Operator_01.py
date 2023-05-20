7a9e521e463d15b2cbd6bc9bf561dc45e14f1c59
manoj <khamitkarmanoj@gmail.com>
2023-05-20 23:06:39 +0530

    Arithmetic_Operators

---
+++ Operators/Operator_01.py
@@ -0,0 +1,28 @@
+val1 = 5
+val2 = 6
+#addition operator
+result = val1 +val2
+print("Addition is :",result)
+
+#subtraction operator
+result = val1 -val2
+print("Subtraction is :",result)
+
+#multiplication operator
+result = val1*val2
+print("multiplication is : ",result)
+
+#Division operator
+result =val1 / val2
+print("Division is: ",result)
+
+#Modulus operator 
+result = val1 % val2
+print("Modulus is : ",result)
+
+#Exponentiation operator
+result = val1 ** val2
+print("Exponentiation is : ",result)
+ #floor division 
+result = val1 // val2 
+print("Floor division is : ",result)
