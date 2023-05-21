2e8bbe71220318dbd58ccdc6778f445f9566513c
manoj <khamitkarmanoj@gmail.com>
2023-05-21 12:13:57 +0530

    Bitwise_Operators

---
+++ Operators/Operator_04.py
@@ -0,0 +1,24 @@
+#Bitwise operators
+a =10
+b =4
+#bitwise AND operator
+print("a & b= ",a&b)
+#bitwise OR operator
+print("a | b = ",a|b)
+#bitwise NOT operator
+print("~a =",~a)
+#bitwise XOR operator
+print("a ^ b = ",a ^ b)
+
+#shift operator
+a =10
+b =-10
+#right shift operator
+print("a >> 1 = ",a >>1)
+print("b >> 1 =", b >>1)
+
+a =5
+b =-10
+#bitwise left shift operator
+print( "a << 1 =",a <<1)
+print("b <<1 =",b<<1)
