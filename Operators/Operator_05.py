eaca2554ecd663511ecc628c18dfa76b48462d00
manoj <khamitkarmanoj@gmail.com>
2023-05-21 13:01:20 +0530

    Assignment_Operator

---
+++ Operators/Operator_05.py
@@ -0,0 +1,57 @@
+#Assignment operator
+a =5
+b =7
+c =a+b #Assign 
+print(c)
+
+a +=b #Add and Assign  
+print(a)
+
+a -= b #Subtract and Assign
+print(a)
+
+a *=b # #Multiply and Assign 
+print(a)
+
+a /=b #Divide and Assign
+print(a)
+
+a %= b #Modulus and Assign
+print(a)
+
+a = 3
+b =5
+a //=b # FLoor Division and Assign
+print(a)
+
+a = 3
+b =5
+a **=b #Exponent and Assign 
+print(a)
+
+a = 3
+b =5
+a &= b #Bitwise And and Assign
+print(a)
+
+a = 3
+b =5
+a |= b #Bitwise OR and Assign
+print(a)
+
+a = 3
+b =5
+a ^= b #Bitwise xOR and Assign
+print(a)
+
+a = 3
+b =5
+a >>= b #Bitwise right shift and Assign
+print(a)
+
+a = 3
+b =5
+a <<= b #Bitwise left shift and Assign
+print(a)
+
+
