15d781d6b0054b8805b8bda2c8a2c43a6ca020ab
manoj <khamitkarmanoj@gmail.com>
2023-05-23 23:54:50 +0530

    sum of array

---
+++ Array_Programs/Program_01.py
@@ -0,0 +1,17 @@
+# To find sum of elements in given array
+def _sum(arr):
+    sum =0
+    for i in arr:
+        sum +=i
+    return(sum)
+
+#main function
+if __name__ == "main__":
+    #input values to list
+    arr = [12,3,4,15]
+    #calculating length of array
+    n = len(arr)
+    #calling function ans store the sum in ans
+    ans =_sum(arr)
+    #display sum
+    print('Sum of the arrayi is ',ans)
