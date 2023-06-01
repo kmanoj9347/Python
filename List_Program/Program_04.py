c154e5895663b1840994a738e1e5e992ba4698c0
manoj <khamitkarmanoj@gmail.com>
2023-06-01 20:47:41 +0530

    Length of a list

---
+++ List_Program/Program_04.py
@@ -0,0 +1,10 @@
+# Finding the length of list using navie method
+
+#Initializing list
+test_list =[1,4,5,7,8]
+print("The list is : ",(test_list))
+#Finding length of list using loop
+counter = 0
+for i in test_list:
+    counter +=1
+print("Length of list using navie method is : ",(counter))
