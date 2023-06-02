5028ec01f59917489b764eae7aebe401f8c2cf9c
manoj <khamitkarmanoj@gmail.com>
2023-06-02 22:07:30 +0530

    element exist in list or not

---
+++ List_Program/Program_06.py
@@ -0,0 +1,9 @@
+# To check if elements exists in list or not.
+lst = [1,3,56,78,3,7,45,9,2]
+#Checking if element is present in the list or not
+i=7
+#if element present then return exists otherwise not exist
+if i in lst:
+    print("exist")
+else:
+    print("not exist")
