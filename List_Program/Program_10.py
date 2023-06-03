c4479e3fad2e41792857a342bd943fa7ef584095
manoj <khamitkarmanoj@gmail.com>
2023-06-03 18:01:16 +0530

    reversing a list

---
+++ List_Program/Program_10.py
@@ -0,0 +1,5 @@
+#reversing a list using the reversed() and reverse() built-in function
+lst=[14,65,8,3,88,34,99,6]
+lst.reverse()
+print("Using reverse()",lst)
+print("Using reversed()",list(reversed(lst)))
