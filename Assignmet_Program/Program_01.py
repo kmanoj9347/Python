c1cab0896ab0f38157dd46578621ae7a1d318239
manoj <khamitkarmanoj@gmail.com>
2023-06-04 23:42:13 +0530

    Arithmetic_Operations

---
+++ Assignmet_Program/Program_01.py
@@ -0,0 +1,11 @@
+# Write a python script to perform basic arithmetic operations 
+# on two values which are accepted from the user 
+num1=int(input("Enter a number1: "))
+num2=int(input("Enter a number2: "))
+print("Addition of {} and {} is {}".format(num1,num2,(num1+num2)))
+print("Subtraction of {} from {} is {}".format(num1,num2,(num1-num2)))
+print("Multiplication of {} with {} is {}".format(num1,num2,(num1*num2)))
+print("Division of {} by {} is {}".format(num1,num2,(num1/num2)))
+print("Modulus of {} by {} is {}".format(num1,num2,(num1%num2)))
+print("Floor Division of {} by {} is {}".format(num1,num2,(num1//num2)))
+print("Exponent of {} to the power of {} is {}".format(num1,num2,(num1**num2)))
