a382122db210074ae35ac61b4f589e35612274b6
manoj <khamitkarmanoj@gmail.com>
2023-06-10 20:41:11 +0530

    gcd

---
+++ Assignmet_Program/Program_11.py
@@ -0,0 +1,10 @@
+# gcd of two numbers using recursive.
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+GCD=gcd(a,b)
+print("GCD is: ",GCD)
