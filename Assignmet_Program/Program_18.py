19d71483cf5d762dd6d30cb58ba66107b0e70447
manoj <khamitkarmanoj@gmail.com>
2023-06-20 19:52:49 +0530

    turtle

--- Assignmet_Program/Program_18.py
+++ Assignmet_Program/Program_18.py
@@ -1,24 +1,30 @@
 import turtle
 
-#create a turtle object
-my_turtle = turtle.Turtle()
+# Setup turtle window
+window = turtle.Screen()
+window.setup(500, 500)  # Set the window size
+window.title("Character 'A'")
 
-#Set the turtle speed (optional)
-my_turtle.speed(1)
+# Create turtle object
+pen = turtle.Turtle()
+pen.speed(1)  # Set the drawing speed
 
-#Move the turtle to the starting position
-my_turtle.penup()
-my_turtle.goto(-100,0) 
-my_turtle.pendown()
+# Absolute positioning of the turtle
+pen.penup()
+pen.goto(-100, 0)  # Starting position of 'A'
+pen.pendown()
 
 # Draw the character 'A'
-my_turtle.goto(-50,100)
-my_turtle.goto(0,0)
-my_turtle.goto(0,50)
-my_turtle.goto(0,50)
-my_turtle.goto(-50,100)
+pen.left(75)
+pen.forward(200)
+pen.right(150)
+pen.forward(200)
+pen.backward(80)
+pen.right(105)
+pen.forward(62)
 
-#Hide the turtle
-my_turtle.hideturtle()
+# Hide the turtle
+pen.hideturtle()
 
+# Keep the window open until it's closed manually
 turtle.done()
