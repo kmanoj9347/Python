df2784e008b28827289fb9ee5d04af13a635b6ca
manoj <khamitkarmanoj@gmail.com>
2023-06-28 10:38:11 +0530

    tkinter_label

--- tkinter/tkinter_03.py
+++ tkinter/tkinter_03.py
@@ -6,9 +6,10 @@ top.geometry("450x300")
 
 #the label for username
 username = Label(top,text="Username").place(x=40,y=60)
+
 # the label for userpassword.
 userpassword=Label(top,text="Password").place(x=40,y=100)
 submit_button= Button(top,text='Submit').place(x=40,y=130)
-#username_inputarea = Entry(top,width=30).place(x=110,y=60)
-#userpassword_inputarea = Entry(top,width=30).place(x=110,y=100)
+username_inputarea = Entry(top,width=30).place(x=110,y=60)
+userpassword_inputarea = Entry(top,width=30).place(x=110,y=100)
 top.mainloop()
